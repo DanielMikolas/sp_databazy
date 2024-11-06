cc11425ec0f65d967b2d571d13783b17ceaa8105
Daniel Mikoláš <danielpc@MacBook-Pro-uzivatela-Daniel-2.local>
2024-11-06 17:45:06 +0100

    Initial commit

---
+++ src/test/java/com/sp_databazy/demo/SpDatabazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp_databazy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpDatabazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
