6510aae7d79528447a1e08f87581abb340985e01
DanielMikolas <mikolas@stud.uniza.sk>
2024-11-11 10:19:43 +0100

    oprava pripojenia a pridanie dependencies

--- src/main/java/com/sp_databazy/demo/SpDatabazyApplication.java
+++ src/main/java/com/sp_databazy/demo/SpDatabazyApplication.java
@@ -1,16 +1,16 @@
 package com.sp_databazy.demo;
 
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-
-
+import org.springframework.context.annotation.Configuration;
 
 
 @SpringBootApplication
-
+@Configuration
 public class SpDatabazyApplication {
 
 	public static void main(String[] args) {
